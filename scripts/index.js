e9972c03b48e45f8928fe22f22afa06c1e877a1f
Yury Ambartsumyan <ambarcumyanjuriy@yandex.ru>
2022-04-06 21:34:56 +0300

    Начальное состояние

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const TODO_LIST = [
+    { title: 'Погладить кота' },
+    { title: 'Изучить Javascript' },
+    { title: 'Полить цветы' },
+    { title: 'Купить еды' },
+    { title: 'Погулять в парке' },
+];
